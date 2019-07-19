d07f8ee814d6093176eb4e9e3cc7a34ea282ed39
AnilreddyMalipeddi1 <anilreddy.malipeddi@thrymr.net>
2019-07-19 13:48:57 +0530

    For loops

---
+++ src/main/java/JavaForEachLoop.java
@@ -0,0 +1,9 @@
+public class JavaForEachLoop {
+    public static void main(String[] args) {
+        int a[]={10,20,30,40};
+        for (int b:a) {
+            System.out.print(b);
+        }
+
+    }
+}
